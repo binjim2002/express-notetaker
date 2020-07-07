963758e865cc9d4e0d6a8d11911d81e0b20d1b58
binjim2002 <binjim2002@yahoo.fr>
2020-07-06 21:45:02 -0400

    test

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -2,27 +2,28 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
+
+
 const dbPath = function(){
     return path.join(__dirname+'../../db/db.json');
 }
 
-const getDbContent = function() {
-   const dg = fs.readFilesSync(dbPath(), 'utf8');
-   return JSON.parse(db);
- 
+const getDbContent = function(){
+    const db = fs.readFileSync(dbPath(),'utf8');
+    return JSON.parse(db);
 }
 
 const writeDbContent = function(data){
     fs.writeFile(dbPath(),JSON.stringify(data), err=> {
         if(err){
             console.log('There was an issue');
-        
+            
         }
     });
-
 }
 
 router.get('/api/notes', function(req,res){
+    
     res.send(getDbContent());
 })
 
@@ -31,22 +32,20 @@ router.post('/api/notes', function(req,res){
     // add posted object to data
     req.body.id = Math.random().toString(36).substring(2,9)
     data.push(req.body)
-    // write to db json
+    // write to db.json
     writeDbContent(data);
-    //  return complete data
+    // return complete data
     res.send(data);
 })
 
-router.delete('/api/notes/:id',function(req,res){
+router.delete('/api/notes/:id',function(req, res){
     const data = getDbContent();
     const newData = data.filter(note => {
         return note.id !== req.params.id
-    
-    
     })
     writeDbContent(newData);
     res.send(newData)
+
 })
 
 module.exports = router;
-    
