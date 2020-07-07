f493ce9e26dbf8f256cc34ec72eb92064eca6c3e
binjim2002 <binjim2002@yahoo.fr>
2020-07-07 08:55:06 -0400

    heroku deployement

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(bodyParser.text());
+
+app.use(require('./routes/html-routes.js'))
+app.use(require('./routes/api-routes.js'))
+
+app.listen(PORT, function(){
+    console.log('server running at http://localhost:'+PORT);
+})
